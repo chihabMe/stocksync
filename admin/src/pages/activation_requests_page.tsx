56372bd9f403e85c636c08fbb4c5a2dde67772cc
chihabMe <chihab.mg.me@gmail.com>
2024-02-27 22:23:57 +0100

    added some pages to the admin project

---
+++ admin/src/pages/activation_requests_page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ActivationRequestsPage = () => {
+  return (
+    <div>activation_requests_page</div>
+  )
+}
+
+export default ActivationRequestsPage;
---
+++ admin/src/pages/complains_page.tsx
@@ -0,0 +1,8 @@
+
+const ComplainsPage = () => {
+  return (
+    <div>complains_page</div>
+  )
+}
+
+export default ComplainsPage;
