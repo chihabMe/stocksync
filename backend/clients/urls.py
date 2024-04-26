7d6e4ce6b179dc332ce38504ee4e4a3a1d1e7ce6
chihabMe <chihab.mg.me@gmail.com>
2024-04-26 21:37:25 +0100

    added client manager view+serialier

--- backend/clients/urls.py
+++ backend/clients/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import ClientsListView
+from .views import ClientsListView,ClientManagerView
 
 app_name = "clients"
 
 urlpatterns = [
     path("",ClientsListView.as_view(),name="clients"),
+    path("manage/<str:id>/",ClientManagerView.as_view(),name="client-manager"),
 ]
--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from .serializers import ClientSerialize
+from .serializers import ClientManagerSerializer
 from accounts.serializers import UserSerializer
 from common.permissions import IsAdmin
 from django.contrib.auth import get_user_model
+from rest_framework.generics import RetrieveUpdateDestroyAPIView  
+from .models import ClientProfile
+from django.shortcuts import get_object_or_404
 
 User  = get_user_model()
 
@@ -13,3 +16,11 @@ class ClientsListView(ListAPIView):
     permission_classes = [IsAdmin]
     def get_queryset(self):
         return User.objects.filter(user_type=User.UserTypesChoices.CLIENT)
+
+class ClientManagerView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ClientManagerSerializer
+    queryset = ClientProfile.objects.all()
+    lookup_field = "id"
+    permission_classes = [IsAdmin]
+    def get_object(self):
+        return get_object_or_404(ClientProfile,user__id=self.kwargs.get("id"))
