1149b63a4dabac1ef7a4e070bc5c4908f39672bb
chihabMe <chihab.mg.me@gmail.com>
2024-05-09 22:42:23 +0100

    merged the old app with the new one

--- app/android/app/src/main/kotlin/com/example/stocksync/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stocksync
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ app/lib/constants.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 123, 52, 255);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color.fromARGB(255, 146, 62, 255),
+    Color.fromARGB(255, 155, 67, 255)
+  ],
+);
+const kSecondaryColor = Color.fromARGB(255, 151, 151, 151);
+const kTextColor = Colors.black;
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+const headingStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding: const EdgeInsets.symmetric(vertical: 10),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(11),
+    borderSide: const BorderSide(color: kTextColor),
+  );
+}
